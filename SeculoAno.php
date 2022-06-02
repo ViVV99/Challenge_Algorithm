dc46a069731cd6b3a68a4968ee017daf3318641c
ViVV99 <vitorvv99@gmail.com>
2022-06-01 23:12:30 -0300

    add: error treatment and persistence

--- SeculoAno.php
+++ SeculoAno.php
@@ -4,17 +4,22 @@ function seculoAno($year) {
     return intval(($year-1)/100)+1;
 }
 
-$year = readline('Digite o ano: ');
+while(true) {
+    $year = readline('Digite o ano: ');
 
-try {
-    if($year <= 0)
-        throw new Exception('O ano deve ser maior que 0');
-    if($year >= 100000)
-        throw new Exception('O valor limite é até 100000');
+    try {
+        if(!is_numeric($year)){
+            throw new Exception('Por favor, informe um número');
+        }
+        if($year <= 0)
+            throw new Exception('O ano deve ser maior que 0');
+        if($year >= 100000)
+            throw new Exception('O valor limite é até 100000');
     
-    echo 'O século deste ano é ' . seculoAno($year) . "\n";
+        echo 'O século deste ano é ' . seculoAno($year) . "\n";
+        break;
 
-} catch(Exception $error) {
-    echo $error->getMessage() . "\n";
-    exit(1);
+    } catch(Exception $error) {
+        echo $error->getMessage() . "\n";
+    }
 }
